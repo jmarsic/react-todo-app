9167d29952f4b01318f3339401c0fa4a2892d448
jmarsic <jm46040@unist.hr>
2022-09-26 20:51:51 +0200

    Added header and button components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Todo App</h1>
+      <Button label="Add" />
+    </header>
+  );
+};
+
+export default Header;
